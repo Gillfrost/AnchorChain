c5304740dc7f3c59713b0c6f17f9f1c6a2aa998d
André Gillfrost <28143870+gillfrost@users.noreply.github.com>
2019-01-16 07:55:47 +0100

    Document anchored methods

--- AnchorChain/Classes/Anchored.swift
+++ AnchorChain/Classes/Anchored.swift
@@ -5,11 +5,30 @@ import UIKit
 
 public extension UIView {
 
+    /**
+     Constrains a variable number of anchors to match superview.
+
+     - Parameter anchors: `.top, .left, .bottom, .right` by default.
+
+     - Returns: The receiver.
+    */
     func anchored(_ anchors: Anchor...) -> Self {
         _ = anchor(anchors)
         return self
     }
 
+    /**
+     Constrains a variable number of anchors to match given view.
+
+     - Note:
+     **Receiver is automatically added as subview to given view, if receiver is without superview.**
+
+     - Parameters:
+       - anchors: `.top, .left, .bottom, .right` by default.
+       - view: The view to match.
+
+     - Returns: The receiver.
+     */
     func anchored(_ anchors: Anchor..., to view: UIView) -> Self {
         _ = anchor(anchors, to: view)
         return self
