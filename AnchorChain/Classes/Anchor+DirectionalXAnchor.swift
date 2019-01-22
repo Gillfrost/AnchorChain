80ae29ddf50c8c708b6853427db153dc7359cf24
André Gillfrost <28143870+gillfrost@users.noreply.github.com>
2019-01-22 21:41:50 +0100

    Enable anchor to layout guide

---
+++ AnchorChain/Classes/Anchor+DirectionalXAnchor.swift
@@ -0,0 +1,51 @@
+//  Copyright (c) 2018 André Gillfrost
+//  Licensed under the MIT license
+
+import UIKit
+
+public extension UIView {
+
+    enum DirectionalXAnchor: CaseIterable { case leading, trailing, centerX }
+
+    /**
+     Constrains an anchor to given anchor in given view.
+
+     - Parameters:
+     - anchor:        The directional horizontal anchor in the receiver.
+     - relation:      `.equal` by default.
+     - otherAnchor:   The directional horizontal anchor to align with.
+     - otherView:     The view to align with.
+     - priority:      `.required` by default.
+     - isActive:      `true` by default.
+
+     - Returns: The created constraint.
+     */
+    @discardableResult
+    func anchor(_ anchor: DirectionalXAnchor,
+                to otherAnchor: DirectionalXAnchor,
+                of otherView: UIView,
+                priority: UILayoutPriority = .required,
+                isActive: Bool = true) -> NSLayoutConstraint {
+
+        disableAutoresizing()
+
+        return self.anchor(for: anchor)
+            .constraint(equalTo: otherView.anchor(for: otherAnchor))
+            .priority(priority)
+            .isActive(isActive)
+    }
+}
+
+private extension Anchorable {
+
+    func anchor(for anchor: UIView.DirectionalXAnchor) -> NSLayoutXAxisAnchor {
+        switch anchor {
+        case .leading:
+            return leadingAnchor
+        case .trailing:
+            return trailingAnchor
+        case .centerX:
+            return centerXAnchor
+        }
+    }
+}
---
+++ AnchorChain/Classes/Anchor+YAnchor.swift
@@ -0,0 +1,52 @@
+//  Copyright (c) 2018 André Gillfrost
+//  Licensed under the MIT license
+
+import UIKit
+
+public extension UIView {
+
+    enum YAnchor: CaseIterable { case top, bottom, centerY }
+
+    /**
+     Constrains an anchor to given anchor in given view.
+
+     - Parameters:
+     - anchor:        The vertical anchor in the receiver.
+     - relation:      `.equal` by default.
+     - otherAnchor:   The vertical anchor to align with.
+     - otherView:     The view to align with.
+     - priority:      `.required` by default.
+     - isActive:      `true` by default.
+
+     - Returns: The created constraint.
+     */
+    @discardableResult
+    func anchor(_ anchor: YAnchor,
+                _ relation: NSLayoutRelation = .equal,
+                to otherAnchor: YAnchor,
+                of otherView: UIView,
+                priority: UILayoutPriority = .required,
+                isActive: Bool = true) -> NSLayoutConstraint {
+
+        disableAutoresizing()
+
+        return self.anchor(for: anchor)
+            .constraint(with: relation, to: otherView.anchor(for: otherAnchor))
+            .priority(priority)
+            .isActive(isActive)
+    }
+}
+
+private extension Anchorable {
+
+    func anchor(for anchor: UIView.YAnchor) -> NSLayoutYAxisAnchor {
+        switch anchor {
+        case .top:
+            return topAnchor
+        case .bottom:
+            return bottomAnchor
+        case .centerY:
+            return centerYAnchor
+        }
+    }
+}
