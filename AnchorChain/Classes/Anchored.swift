a65d2ea953f1d3b0953db57a7bd7a6c9d1a62c8a
André Gillfrost <28143870+gillfrost@users.noreply.github.com>
2019-01-23 21:39:41 +0100

    Enable layout guide anchoring for anchored API

--- AnchorChain/Classes/Anchored.swift
+++ AnchorChain/Classes/Anchored.swift
@@ -18,6 +18,20 @@ public extension UIView {
     }
 
     /**
+     Constrains a variable number of anchors to match given layout guide of superview.
+
+     - Parameters:
+       - anchors:     `.top, .left, .bottom, .right` by default.
+       - layoutGuide: The layout guide to match.
+
+     - Returns: The receiver.
+     */
+    func anchored(_ anchors: Anchor..., to layoutGuide: LayoutGuide) -> Self {
+        _ = anchor(anchors, to: layoutGuide)
+        return self
+    }
+
+    /**
      Constrains a variable number of anchors to match given view.
 
      - Note:
@@ -31,6 +45,24 @@ public extension UIView {
      */
     func anchored(_ anchors: Anchor..., to view: UIView) -> Self {
         _ = anchor(anchors, to: view)
+        return self
+    }
+
+    /**
+     Constrains a variable number of anchors to match given layout guide of given view.
+
+     - Note:
+     **Receiver is automatically added as subview to given view, if receiver is without superview.**
+
+     - Parameters:
+     - anchors:     `.top, .left, .bottom, .right` by default.
+     - layoutGuide: The layout guide to match.
+     - view:        The view to match.
+
+     - Returns: The receiver.
+     */
+    func anchored(_ anchors: Anchor..., to layoutGuide: LayoutGuide, of view: UIView) -> Self {
+        _ = anchor(anchors, to: layoutGuide, of: view)
         return self
     }
 }
