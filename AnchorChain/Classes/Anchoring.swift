8975af87e7397248231e76fb5bceebff846bf999
André Gillfrost <28143870+gillfrost@users.noreply.github.com>
2019-01-27 18:05:18 +0100

    Enable layout guide anchoring for anchoring API

--- AnchorChain/Classes/Anchoring.swift
+++ AnchorChain/Classes/Anchoring.swift
@@ -11,9 +11,9 @@ public extension UIView {
      Passing `.size` as anchor will constrain both width and height to given constant.
 
      - Parameters:
-     - anchor:    The dimension to constrain, `.width`, `.height` or `.size`.
-     - constant:  The constant.
-     - priority:  `.required` by default.
+       - anchor:    The dimension to constrain, `.width`, `.height` or `.size`.
+       - constant:  The constant.
+       - priority:  `.required` by default.
 
      - Returns: The receiver.
      */
@@ -27,13 +27,55 @@ public extension UIView {
     }
 
     func anchoring(_ view: UIView) -> Self {
+
         _ = view.anchor([], to: self)
 
         return self
     }
 
+    /**
+     Constrains given view to given layout guide of receiver.
+
+     - Note:
+       **Given view is automatically added as subview to receiver, if it is without superview.**
+
+     - Parameters:
+       - view:        The view whose edges should match given layout guide of receiver.
+       - layoutGuide: The layout guide in receiver to match.
+
+     - Returns: The receiver.
+     */
+    func anchoring(_ view: UIView, to layoutGuide: LayoutGuide) -> Self {
+
+        _ = view.anchor([], to: layoutGuide, of: self)
+
+        return self
+    }
+
     func anchoring(_ a1: Anchor, _ aX: Anchor..., of view: UIView) -> Self {
+
         _ = view.anchor([a1] + aX, to: self)
+
+        return self
+    }
+
+    /**
+     Constrains one or more anchors of given view to match given layout guide of receiver.
+
+     - Note:
+       **Given view is automatically added as subview to receiver, if it is without superview.**
+
+     - Parameters:
+       - a1:          An anchor.
+       - aX:          More anchors.
+       - view:        The view to match.
+       - layoutGuide: The layout guide in receiver to match.
+
+     - Returns: The receiver.
+     */
+    func anchoring(_ a1: Anchor, _ aX: Anchor..., of view: UIView, to layoutGuide: LayoutGuide) -> Self {
+
+        _ = view.anchor([a1] + aX, to: layoutGuide, of: self)
 
         return self
     }
